0456d6ff695a7fdae6bb685651778c09b41e5f96
аппле <apple@MacBook-Air-apple.local>
2024-10-02 21:39:35 +0500

    new javascript file is open

---
+++ js/index.js
@@ -0,0 +1,222 @@
+// 1-masala
+// for (let i = 1 ; i <= 5 ; i++) {
+//     console.log(i);
+// }
+
+// 2-masala
+// for (let i = 1 ; i <= 20 ; i++) {
+//     console.log(i);
+// }
+
+// 3-masala
+// let num1 =+prompt ("Sonni kriting") ;
+// num2 = num1 ** 2;
+
+// for (let i = 1; i <= num1; i++){
+//     console.log(num2);
+// }
+
+
+//  4-masala
+// for (let i = 2 ; i <= 10; i+=2 ) {
+//     console.log(i);
+// }
+
+// 5-masala
+
+// for (let i = 1 ; i <= 15 ; i+= 2) {
+//     console.log(i);
+// }
+
+// 6-masala
+
+// let num1 = +prompt("1- sonni kiriting") ;
+// let num2 = +prompt("2- sonni kiriting") ;
+// let num3 = +prompt("3- sonni kiriting") ;
+// let num4 = +prompt("4- sonni kiriting") ;
+// let num5 = +prompt("5- sonni kiriting") ;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+
+// 7-masala
+
+// for (let i = 15 ; i <= 5 ; i--) {
+// console.log(i);
+// }
+
+// 8-masala
+// let num = +prompt("Sonni kiriting");
+// let sum = 1
+// for ( let i = 1; i <= num ; i++ ) {
+//     sum *= i
+// }
+// console.log(sum);
+
+// 9-masala
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let num4 = +prompt("4-sonni kiriting");
+// let num5 = +prompt("5-sonni kiriting");
+// let num6 = +prompt("6-sonni kiriting");
+// let num7 = +prompt("7-sonni kiriting");
+// let num8 = +prompt("8-sonni kiriting");
+// let num9 = +prompt("9-sonni kiriting");
+// let num10 = +prompt("10-sonni kiriting");
+
+// let res = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+// console.log(res);
+
+// 10-masala
+
+// for (let i = 1 ; i <= 50 ; i++) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// 11-masala
+
+// for (let i = 100 ; i >= 1 ; i--) {
+//     console.log(i);  
+// }
+
+// 12-masala
+
+// let sum = 0
+// for (let i = 1 ; i<=100 ; i++){
+//     sum = sum +i
+// }
+// console.log(sum);
+
+// 13-masala
+
+// let num1 = +prompt("1- sonni kiriting") ;
+// let num2 = +prompt("2-sonni kiriting") ;
+
+// for (let i = num1 ; i <= num2 ; i++ ) {
+//     console.log(i);
+// }
+
+// 14-masala
+
+// let num =+prompt("Sonni kiriting");
+// for( let i = 0 ; i<=num ; i++) {
+//     if (num % i == 0)  
+// console.log(i);
+// }
+
+// 15-masala
+
+// for (let i = 1; i <=20; i++){
+//    console.log(i+i*i);
+// }
+
+// 16-masala
+// for (let i = 1 ; i<=100 ; i++){
+//     if (i % 7 == 0) 
+//     console.log(i);
+// }
+
+//  17-masala
+//  sum = 1
+//   for (let i = 1 ; i<=10 ; i++){ for (
+// let another = 1 ; another<=10 ; i++  ) 
+// console.log(i+another);
+//   }
+// 
+
+// 18-masala
+
+// let num = ("Son kiriting") ;
+// sum = 0;
+
+// for (let i = 1 ; i <= num ; i++) {
+//      if (num % i == 0) {
+//      sum = sum +i
+//      }
+// }
+// console.log(sum);
+
+// // 19-masala
+// sum = 0
+// for (let i = 1 ; i <=50 ; i++) { if (i % 2 == 1) { sum + i
+// }
+// console.log(i);
+// }
+
+// 20-masala
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+
+// for (let i = num1 ; i<=num2 ; i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 21-masala
+// sum = 0
+// for (let i = 1 ; i <=100 ; i++ ) {
+//     if (i % 2 == 1) {
+//         sum = sum + i
+//     } 
+// }
+// console.log(sum);
+
+// 22-masala
+sum = 1
+let num1 = +prompt("sonni kiritng") ;
+for (let i = 1; i<=num1; i++){
+    sum = sum * i;
+}
+console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
